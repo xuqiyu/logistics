7e544c8f5f672d8a94ac4dd573f54c897d9d2c08
xuqiyu <xuqiyu012@126.com>
2014-10-09 13:39:07 +0800

    123

--- closeservice.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-curl http://localhost:7001/switcher/deliveryInnerSwitcher.do?switcher=hasCOD&value=false
-curl http://localhost:7001/switcher/deliveryInnerSwitcher.do?switcher=openEnsureNewVersion&value=false
-curl http://localhost:7001/switcher/deliveryInnerSwitcher.do?switcher=openOrderToSend&value=false
-curl http://localhost:7001/switcher/deliveryInnerSwitcher.do?switcher=openNextDayUpTo&value=false
-curl http://localhost:7001/switcher/deliveryInnerSwitcher.do?switcher=openWlbNextDay&value=false
